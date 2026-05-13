433d583c9adde3eaa6061edad1ac59c780065fe4
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2026-05-13 11:51:17 +0530

    Learned about routing, nested routing, and params

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,7 @@
+const UserDetails = async ({ params } : { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+
+  return <div>Details of user number {id}</div>;
+};
+
+export default UserDetails;
