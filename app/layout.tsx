ac95676fbcb6f7967124eaf0af00c12e21dd1929
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2026-05-13 19:44:07 +0530

    Completed the Home page ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRays";
-
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -18,7 +16,7 @@ const martianMono = Martian_Mono({
 
 export const metadata: Metadata = {
   title: "DevEvent",
-  description: "The Hub For Every Dev Event You Must'nt Miss",
+  description: "The Hub for Every Dev Event You Mustn't Miss",
 };
 
 export default function RootLayout({
@@ -27,18 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn(
-        "min-h-screen",
-        "antialiased",
-        schibstedGrotesk.variable,
-        martianMono.variable,
-        "font-sans",
-        geist.variable,
-      )}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="en">
+      <body
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+      >
+        <Navbar />
+
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
           <LightRays
             raysOrigin="top-center-offset"
@@ -47,12 +39,13 @@ export default function RootLayout({
             lightSpread={0.9}
             rayLength={1.4}
             followMouse={true}
-            mouseInfluence={0.04}
-            noiseAmount={0}
+            mouseInfluence={0.02}
+            noiseAmount={0.0}
             distortion={0.01}
           />
         </div>
-        {children}
+
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import EventCard from "@/components/EventCard";
 import ExploreButton from "@/components/ExploreButton";
+import { events } from "@/lib/constants";
 
 const Page = () => {
   return (
     <section>
-      <h1 className="text-center mt-24">
+      <h1 className="text-center">
         The Hub For Every Dev <br />
         Event You Can't Miss
       </h1>
@@ -11,6 +13,17 @@ const Page = () => {
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
       <ExploreButton />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
