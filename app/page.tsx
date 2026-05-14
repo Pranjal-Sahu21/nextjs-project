c31d06d6250027dc9489168f2546dcf3eda80d9e
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2026-05-15 02:03:23 +0530

    Remove commented fetch logic and adjust event mapping in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import EventCard from "@/components/EventCard";
 import ExploreButton from "@/components/ExploreButton";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
+import { events } from "@/lib/constants";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const Page = async () => {
   'use cache';
   cacheLife('hours');
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
 
   return (
     <section>
@@ -24,7 +25,7 @@ const Page = async () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none">
-          {events && events.length > 0 && events.map((event: IEvent) => (
+          {events && events.length > 0 && events.map((event) => (
             <li key={event.title}>
               <EventCard {...event} />
             </li>
